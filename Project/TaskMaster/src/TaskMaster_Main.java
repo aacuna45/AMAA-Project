69ca782a6393c13f13889d17dd4e315341796120
alenrtan <113738058+alenrtan@users.noreply.github.com>
2023-03-28 01:13:32 -0600

    Loading the FXML file to work with Main

--- Project/TaskMaster/src/TaskMaster_Main.java
+++ Project/TaskMaster/src/TaskMaster_Main.java
@@ -1,7 +1,10 @@
 package Project.TaskMaster.src;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -10,13 +13,16 @@ import javafx.stage.Stage;
 public class TaskMaster_Main extends Application{
     
     public static void main(String args[]){
+        TaskMaster_Reminders test = new TaskMaster_Reminders("Test Reminder", "This is a test", "12-03-2023");
+        System.out.println("\n\n\n" + test.toString() + "\n\n\n");
         launch(args);
+
+        
     }
 
     @Override
     public void start(Stage stage) throws Exception{
-        //Stage stage = new Stage();
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("TaskMaster_Main.fxml"));
         Scene scene = new Scene(root, Color.DARKGREY);
         Image icon = new Image("Project/TaskMaster/src/TaskMaster_icon_trial.png");
         
